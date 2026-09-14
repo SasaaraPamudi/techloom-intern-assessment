aff0bd9fbd9bf6b0308b075c99c987355523f823
sasaara <pamudisasaara@gmail.com>
2026-09-15 04:42:23 +0530

    updated vite.config.js

--- task-01/frontend/vite.config.js
+++ task-01/frontend/vite.config.js
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  experimental: {
-    renderBuiltUrl(filename) {
-      return filename;
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 });
