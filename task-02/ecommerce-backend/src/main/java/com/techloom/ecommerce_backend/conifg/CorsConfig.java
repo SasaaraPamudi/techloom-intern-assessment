d5eb3e777c562928a7427c86c982952b61113e28
sasaara <pamudisasaara@gmail.com>
2026-09-15 07:09:00 +0530

    fix: configure global CORS policy to allow Vercel frontend origin

--- task-02/ecommerce-backend/src/main/java/com/techloom/ecommerce_backend/conifg/CorsConfig.java
+++ task-02/ecommerce-backend/src/main/java/com/techloom/ecommerce_backend/conifg/CorsConfig.java
@@ -7,14 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://techloom-intern-assessment-6k2m-qiwiktpwn-sasaara.vercel.app",
+                                "http://localhost:3000",
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
