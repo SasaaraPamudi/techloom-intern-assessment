e82cfa4fd57c8faa5124daa1e75c0a8c13feaab3
sasaara <pamudisasaara@gmail.com>
2026-09-15 07:01:29 +0530

    change the environment variable to VITE_BACKEND_URL

--- task-02/ecommerce-frontend/src/services/api.js
+++ task-02/ecommerce-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8081/api',
 });
 
 export const getProducts = () => API.get('/products');
